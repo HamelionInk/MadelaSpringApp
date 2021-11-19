526c8e325b9421f4da1dba510ab6397a337c2657
HamelionInk <term11term@gmail.com>
2021-11-19 19:33:33 +0300

    interface for service add

--- src/main/java/com/nikitin/mailapp/service/MyMailSender.java
+++ src/main/java/com/nikitin/mailapp/service/MyMailSender.java
@@ -1,4 +1,14 @@
 package com.nikitin.mailapp.service;
 
+import com.nikitin.mailapp.model.Person;
+import org.springframework.mail.SimpleMailMessage;
+
+import java.util.List;
+
 public interface MyMailSender {
+    void sendEmail();
+
+    SimpleMailMessage createEmail(String emailTo, String subject, String message);
+
+    List<Person> getEmail();
 }
